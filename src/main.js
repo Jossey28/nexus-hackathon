ba66798c87b038c90acb174dd7a739c8810827ec
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-06-19 20:35:31 -0700

    work on html

--- src/main.js
+++ src/main.js
@@ -1,11 +1,50 @@
 import kaplay from "kaplay";
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+
+const k = kaplay({
+    // width: 700,
+    // height: 700,
+    background: "#AACCD6",
+    canvas: document.querySelector("#kaplay-game")
+});
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+k.loadSprite("bird", "sprites/bird.png");
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+const bird = add([
+    sprite("bird"),
+    scale(0.25),
+    color(RED),
+
+    anchor("center"),
+    pos(center()),
+    rotate(90),
+])
+
+
+
+scene("display_bird", () => {
+    add([
+
+    ]);
+})
+
+onUpdate(() => {
+    if (isKeyDown("h")) {
+
+    }
+})
+
+onKeyPress("space", () => {
+    debug.log("spaced");
+});
+
+onAdd("shape", () => {
+    debug.log("shaped");
+});
+
+// k.onClick(() => k.addKaboom(k.mousePos()));
+
+k.go("display_bird")
