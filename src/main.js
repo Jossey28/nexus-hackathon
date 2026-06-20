bc1cc430c9a11741e323bed78c35f2eff3e5294e
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-06-20 02:36:01 -0700

    TITLE SCREEN

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import kaplay from "kaplay";
 import "kaplay/global";
 
-const GRAVITY = 200;
+const GRAVITY = 150;
 
-const UP_FORCE = 2000;
-const DOWN_FORCE = 2000;
+const UP_FORCE = 500;
+const DOWN_FORCE = 500;
 
 const k = kaplay({
     width: 700,
     height: 700,
     background: "#AACCD6",
-    canvas: document.querySelector("#kaplay-game")
+    canvas: document.querySelector("#kaplay-game"),
+
+    buttons: {
+        "flyUp": {
+            keyboard: ["up", "w"],
+            mouse: ["left"]
+        },
+        "flyDown": {
+            keyboard: ["down", "d"],
+            mouse: ["right"]
+        }
+    }
 });
 
 loadRoot("./"); // A good idea for Itch.io publishing later
@@ -19,24 +30,8 @@ loadSprite("bird", "sprites/bird.png");
 
 setGravity(GRAVITY);
 
-function createBird() {
-    const bird = add([
-        sprite("bird"),
-        scale(0.25),
-
-        anchor("center"),
-        rotate(90),
-
-        pos(center()),
-        area(),
-        body(),
-    ]);
-
-    return bird
-}
-
-function createBorders() {
-    const floorThickness = 50;
+scene("game_loop", () => {
+    const floorThickness = 25;
     const floorColor = Color.fromHex("#8B5E3C")
 
     const ceiling = [
@@ -51,31 +46,62 @@ function createBorders() {
         color(floorColor),
     ];
 
-    return [ceiling, ground]
-}
+    const bird = add([
+        sprite("bird"),
+        scale(0.25),
 
-scene("game_loop", () => {
-    const [ceiling, ground] = createBorders();
-    const bird = createBird();
+        anchor("center"),
+        rotate(90),
+
+        pos(center()),
+        area(),
+        body(),
+    ]);
+
+    bird.onUpdate(() => {
+        if (bird.pos.y >= (height() - floorThickness) || bird.pos.y <= (floorThickness)) {
+            debug.log("you just died");
+            go("title_screen");
+        }
+    })
 
     add(ceiling);
     add(ground);
 
     onClick(() => addKaboom(mousePos()));
 
-    // onUpdate(() => {
-    //     if (isKeyDown("space")) {
-
-    //     }
-    // });
-
-    onKeyDown("up", () => {
+    onKeyDown("flyUp", () => {
         bird.vel.y -= dt() * UP_FORCE;
     })
 
-    onKeyDown("down", () => {
+    onKeyDown("flyDown", () => {
         bird.vel.y += dt() * DOWN_FORCE;
     })
 })
 
-go("game_loop")
+scene("title_screen", () => {
+    const titleMenu = add([
+        rect(300, 260, { radius: 10 }),
+        color(255, 255, 255),
+        outline(4),
+        anchor("center"),
+        pos(center()),
+    ]);
+
+    titleMenu.add([
+        text("Escape!!", { size: 40 }),
+        color(BLACK),
+        anchor("top"),
+        pos(0, -100),
+    ]);
+
+    titleMenu.button = [
+        ""
+    ]
+
+    onKeyPress(() => {
+        go("game_loop");
+    })
+})
+
+go("title_screen")
